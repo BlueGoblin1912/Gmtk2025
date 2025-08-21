844f5c0585120679a90b6370105358fe37b79fc3
alex <alexwoods1912@gmail.com>
2025-08-21 16:18:22 +0100

    fix

--- loopZ/code/wall.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class Wall(pygame.sprite.Sprite):
-    def __init__(self,pos, groups):
-        super().__init__(groups)
-        self.image = pygame.image.load("../graphics/sprites/wall.png").convert_alpha()
-        self.image = pygame.transform.scale2x(self.image)
-        self.rect = self.image.get_rect(topleft = pos)
--- loopZ/graphics/map/levels/room 8/symbols.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="symbols" tilewidth="32" tileheight="32" tilecount="6" columns="3">
- <image source="../tiles/symbols.png" width="96" height="64"/>
-</tileset>
